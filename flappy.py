3ac987b103327e87e8b792b8a6081057d5e7b6b1
Travis Wheeler <51341303+TravisWheeler130@users.noreply.github.com>
2025-06-21 16:03:10 -0600

    Moving pipes
    pipes are moving, still no collision.

--- flappy.py
+++ flappy.py
@@ -6,9 +6,16 @@ screenTitle = "Flappy Bird"
 
 birdSize = 30
 gravity = 0.5
-jumpStrength = 10
+jumpStrength = 8
 
 class FlappyGame(arcade.Window):
+    pipeList = []
+    pipeSpeed = 2
+    pipeGap = 150
+    pipeWidth = 50
+    pipeSpace = 300
+    numPipes = 3
+
     def __init__(self):
         super().__init__(screenWidth, screenHeight, screenTitle)
         self.birdY = screenHeight // 2
@@ -19,6 +26,7 @@ class FlappyGame(arcade.Window):
     def setup(self):
         self.birdY = screenHeight // 2
         self.birdVelocity = 0
+        self.generatePipes()
 
     def on_draw(self):
         self.clear()
@@ -28,6 +36,17 @@ class FlappyGame(arcade.Window):
             100 + birdSize / 2, self.birdY - birdSize / 2,
             arcade.color.YELLOW
         )
+        for pipe in self.pipeList:
+            x = pipe["x"]
+            gapY = pipe["gapY"]
+            gapHalf = self.pipeGap / 2
+            left = x - self.pipeWidth / 2
+            right = x + self.pipeWidth / 2
+
+            # top pipe
+            arcade.draw_lrbt_rectangle_filled(left, right, gapY + gapHalf, screenHeight, arcade.color.BLACK)
+            # bottom pipe
+            arcade.draw_lrbt_rectangle_filled(left, right, 0, gapY - gapHalf, arcade.color.BLACK)
 
     def update(self, deltaTime):
         print(f"Bird Y: {self.birdY}, Velocity: {self.birdVelocity}")  # DEBUG
@@ -40,11 +59,26 @@ class FlappyGame(arcade.Window):
         elif self.birdY > screenHeight - birdSize / 2:
             self.birdY = screenHeight - birdSize / 2
             self.birdVelocity = 0
+        
+        for pipe in self.pipeList:
+            pipe["x"] -= self.pipeSpeed
+            if pipe["x"] < -self.pipeWidth:
+                pipe["x"] = screenWidth + self.pipeSpace
+                import random
+                pipe["gapY"] = random.randint(100, screenHeight - 100)
 
     def on_key_press(self, key, modifiers):
         if key == arcade.key.SPACE:
             print("Jump!")  # DEBUG
             self.birdVelocity = jumpStrength
+
+    def generatePipes(self):
+        import random
+        self.pipeList.clear()
+        for i in range(self.numPipes):
+            x = screenWidth + i * self.pipeSpace
+            gapY = random.randint(100, screenHeight - 100)
+            self.pipeList.append({"x": x, "gapY": gapY})
 
 if __name__ == "__main__":
     window = FlappyGame()
