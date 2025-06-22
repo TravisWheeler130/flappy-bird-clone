7822ed18fcfb2bc593e66a727c8238af5ff0ea02
Travis Wheeler <51341303+TravisWheeler130@users.noreply.github.com>
2025-06-21 22:26:17 -0600

    working game without stretch

--- flappy.py
+++ flappy.py
@@ -17,6 +17,7 @@ class FlappyGame(arcade.Window):
     numPipes = 3
     score = 0
     bestScore = 0
+    isStarted = False
 
     def __init__(self):
         super().__init__(screenWidth, screenHeight, screenTitle)
@@ -25,20 +26,37 @@ class FlappyGame(arcade.Window):
         arcade.set_background_color(arcade.color.SKY_BLUE)
         self.set_update_rate(1/60)
         self.score = 0
+        self.isStarted = False
+
+        # Sprite
+        self.bird = arcade.Sprite("assets/bird.png", scale=0.125)
+        self.bird.center_x = 100
+        self.bird.center_y = self.birdY
+
+        self.pipe = arcade.Sprite("assets/pipe.png", scale=.5)
+
+        self.spriteList = arcade.SpriteList()
+        self.spriteList.append(self.bird)
+        #self.bird = None
+
+        self.background = arcade.load_texture("assets/background.png")
+        self.backgroundX = 0
+        self.backgroundSpeed = self.pipeSpeed * 0.3
 
     def setup(self):
+        self.bird.center_x = 100
+        self.bird.center_y = self.birdY
         self.birdY = screenHeight // 2
         self.birdVelocity = 0
         self.generatePipes()
+        self.isStarted = False
 
     def on_draw(self):
         self.clear()
-        arcade.draw_triangle_filled(
-            100, self.birdY + birdSize / 2,
-            100 - birdSize / 2, self.birdY - birdSize / 2,
-            100 + birdSize / 2, self.birdY - birdSize / 2,
-            arcade.color.YELLOW
-        )
+        #arcade.draw(self.backgroundX + screenWidth / 2, screenHeight / 2, screenWidth, screenHeight, self.background)
+        #arcade.draw_sprite_rect(self.backgroundX + screenWidth + screenWidth / 2, screenHeight / 2, screenWidth, screenHeight, self.background)
+        self.spriteList.draw()
+        # self.spriteList.draw_hit_boxes()
         for pipe in self.pipeList:
             x = pipe["x"]
             gapY = pipe["gapY"]
@@ -51,11 +69,16 @@ class FlappyGame(arcade.Window):
             # bottom pipe
             arcade.draw_lrbt_rectangle_filled(left, right, 0, gapY - gapHalf, arcade.color.BLACK)
             arcade.draw_text(f"Score: {self.score}    Best: {self.bestScore}", 10, screenHeight - 30, arcade.color.RED, 20)
+            if not self.isStarted:
+                arcade.draw_text("Press SPACE to start", screenWidth / 2, screenHeight / 2, arcade.color.DARK_GREEN, font_size = 20, anchor_x="center")
 
     def update(self, deltaTime):
+        if not self.isStarted:
+            return
         print(f"Bird Y: {self.birdY}, Velocity: {self.birdVelocity}")  # DEBUG
         self.birdVelocity -= gravity
         self.birdY += self.birdVelocity
+        self.bird.center_y = self.birdY
 
         if self.birdY < birdSize / 2:
             self.birdY = birdSize / 2
@@ -83,6 +106,11 @@ class FlappyGame(arcade.Window):
                 pipe["gapY"] = random.randint(100, screenHeight - 100)
                 pipe["scored"] = False
 
+            # background scroll
+            self.backgroundX -= self.backgroundSpeed
+            if self.backgroundX <= -screenWidth:
+                self.backgroundX = 0
+
             # Pipe collision
             pipeLeft = pipe["x"]
             pipeRight = pipe["x"] + self.pipeWidth
@@ -104,7 +132,7 @@ class FlappyGame(arcade.Window):
                 self.setup()
                 self.score = 0
                 break
-            
+
             if pipeLeft < birdRight and pipeRight > birdLeft:
                 if birdTop > pipeBottom or birdBottom < pipeTop:
                     print("Game Over!")
@@ -115,6 +143,8 @@ class FlappyGame(arcade.Window):
 
     def on_key_press(self, key, modifiers):
         if key == arcade.key.SPACE:
+            if not self.isStarted:
+                self.isStarted = True
             print("Jump!")  # DEBUG
             self.birdVelocity = jumpStrength
 
